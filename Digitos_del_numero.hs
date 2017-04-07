19d709aad72dbfde4ea4f9cf83ab32e3d7a74819
Brallan Mendoza <keidragon14@gmail.com>
2017-04-07 17:49:09 -0500

    Update Digitos_del_numero.hs

--- Digitos_del_numero.hs
+++ Digitos_del_numero.hs
@@ -3,4 +3,4 @@ listar 0 = [];
 listar a =listar(div a 10)++[tomarUltimoDigito(a)]
 tomarUltimoDigito::Int->Int
 tomarUltimoDigito a = a-((div a 10)*10)
-main = print $ listar(1000)
+main = print $ listar(1948587)
