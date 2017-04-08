9e28bb2790e0018ac2c0046b280d4acef2face6f
Brallan Mendoza <keidragon14@gmail.com>
2017-04-07 19:10:12 -0500

    Update and rename menorListas.hs to MenorListas.hs

--- menorListas.hs
+++
@@ -1,9 +0,0 @@
---ghc 7.10
-listar::[[Int]]->[Int]
-listar []= []
-listar (x:xs) = [menor(x)]++listar(xs)
-menor::[Int]->Int
-menor n = length n = n
-menor (x:xs) = if x<menor
-
-main = print $ listar([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
