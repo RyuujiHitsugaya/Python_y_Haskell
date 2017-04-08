f846adbc95e3bcd0ebe27fdfa217a80ec536ce52
Brallan Mendoza <keidragon14@gmail.com>
2017-04-07 19:26:48 -0500

    Update SumarPares.hs

--- SumarPares.hs
+++ SumarPares.hs
@@ -1,6 +1,6 @@
-listar::Int->Int
-listar 0 = 0;
-listar a = if ((tomarUltimoDigito(a)`mod`2)==0) then listar(div a 10)+tomarUltimoDigito(a) else listar(div a 10)
+sumar::Int->Int
+sumar 0 = 0;
+sumar a = if ((tomarUltimoDigito(a)`mod`2)==0) then sumar(div a 10)+tomarUltimoDigito(a) else sumar(div a 10)
 tomarUltimoDigito::Int->Int
 tomarUltimoDigito a = a-((div a 10)*10)
-main = print $ listar(198215)
+main = print $ sumar(198215)
